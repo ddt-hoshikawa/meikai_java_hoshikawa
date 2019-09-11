2c01c3ce12a347109ffbe5fa7d881074c6c555f9
ddt-mhoshikawa <ddt.mhoshikawa@gmail.com>
2019-09-11 21:24:40 +0900

    演習1-1

---
+++ meikai_java_20190902/src/e_01/e_01_01.java
@@ -0,0 +1,21 @@
+package e_01;
+
+
+ /*
+  * プログラム中の文の終端を示すセミコロン;が欠如しているとどうなるか。
+  * プログラムをコンパイルして検証せよ
+  * 
+  * @author 干川　美知果
+  * @since　2019.09.02
+  */
+
+public class e_01_01 {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("ほしかわ")
+		
+	}
+
+}
